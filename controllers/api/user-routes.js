0308112554c5566f515f7ea1673bfa74eabf8ee0
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-10 11:33:19 -0500

    Update user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,4 +55,37 @@ router.post('/logout', (req, res) => {
   }
 });
 
+// Route for user signup
+router.post('/signup', async (req, res) => {
+  try {
+    const existingUser = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    // Check if the username is already taken
+    if (existingUser) {
+      res.status(400).json({ message: 'Username already taken. Please choose another username.' });
+      return;
+    }
+
+    // Create a new user
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password, // Assuming you have a hashed password
+    });
+
+    req.session.save(() => {
+      req.session.userId = newUser.id;
+      req.session.username = newUser.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: newUser, message: 'Account created and logged in!' });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
